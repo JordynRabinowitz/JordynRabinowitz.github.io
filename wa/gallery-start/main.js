9c40dc2736bb062772f2b96149bc7e6a44dac12e
Jordyn <jordynjr2004@gmail.com>
2023-11-09 14:08:06 -0700

    new images

--- wa/gallery-start/main.js
+++ wa/gallery-start/main.js
@@ -6,20 +6,20 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const images = ['door.JPG', `door2.JPG`, `jordyn.jpg`, `avatar.JPG`, `outside.JPG`];
 const alts = {
-  'pic1.jpg' : 'Closeup of a human eye',
-  'pic2.jpg' : 'Rock that looks like a wave',
-  'pic3.jpg' : 'Purple and white pansies',
-  'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
-  'pic5.jpg' : 'Large moth on a leaf'
+  'door.JPG' : 'door',
+  'door2.JPG' : 'closeup of door',
+  'jordyn.jpg' : 'jordyn rabinowitz',
+  'avatar.JPG' : 'avatar',
+  'outside.JPG' : 'outside CU'
 }
 
 /* Looping through images */
 
 for (const image of images) {
   const newImage = document.createElement('img');
-  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('src', `../../img/${image}`);
   newImage.setAttribute('alt', alts[image]);
   thumbBar.appendChild(newImage);
   newImage.addEventListener('click', e => {
